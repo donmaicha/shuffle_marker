94bdeaa7cffb3fcb408bd5b79d037c8e2e5570e3
Hfukushima <genmaicha77783@gmail.com>
2019-10-02 20:55:36 +0900

    Initial Commit

---
+++ shuffle_marker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shuffle_marker
+//
+//  Created by 福島拓希 on 2019/10/02.
+//  Copyright © 2019 福島拓希. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ shuffle_markerTests/shuffle_markerTests.swift
@@ -0,0 +1,34 @@
+//
+//  shuffle_markerTests.swift
+//  shuffle_markerTests
+//
+//  Created by 福島拓希 on 2019/10/02.
+//  Copyright © 2019 福島拓希. All rights reserved.
+//
+
+import XCTest
+@testable import shuffle_marker
+
+class shuffle_markerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
