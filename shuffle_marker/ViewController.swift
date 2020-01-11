e4a601c53f55b654e8dbca37b7e395a74db661e4
Hfukushima <genmaicha77783@gmail.com>
2020-01-11 18:30:05 +0900

    コメント修正

--- shuffle_marker/ViewController.swift
+++ shuffle_marker/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     let myView = UIImageView()
     var isShuffle = false
 
+    /// コンストラクタ
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(red: 143/255, green: 180/255, blue: 139/255, alpha: 1.0)
@@ -23,58 +24,58 @@ class ViewController: UIViewController {
         myView.image = UIImage(named: "card_draw")
         view.addSubview(myView)
 
-        //UIGestureのインスタンス
+        // UIGestureのインスタンス
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapped(_:)))
         
-        //UIGestureのデリゲート
+        // UIGestureのデリゲート
         tapGesture.delegate = self as? UIGestureRecognizerDelegate
         
-        //viewに追加
+        // viewに追加
         self.view.addGestureRecognizer(tapGesture)
     }
     
-    //Labelをタップした時の処理
+    /// 画面をタップした時の処理
     @objc func tapped(_ sender: UITapGestureRecognizer) {
-        
-        //タップ直後に中身を実行
+        // タップ直後に中身を実行
         if sender.state == .ended {
-            
-            //Labelをタップしたら、デバッグエリアに"タップされました"を表示
             rotateCard()
-            
         }
         
     }
     
+    /// カードをくるっとひっくり返すアニメーション
     @objc func rotateCard() {
-        //二重タップ防止
+        // 二重タップ防止
         self.view.isUserInteractionEnabled = false
 
         UIView.animate(withDuration: 0.5, animations: {
-            //first transform
+            // first transform
             self.myView.transform = CGAffineTransform(scaleX: 0.01, y: 1.0)
         }, completion: { (finished: Bool) in
             UIView.animate(withDuration: 0.5, animations: {
                 self.changeImge()
-                //second transform
+                // second transform
                 self.myView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
             }, completion:{ (finished: Bool) in
-                //二重タップ防止　解除
+                // 二重タップ防止　解除
                 self.view.isUserInteractionEnabled = true
             })
         })
+        
     }
 
+    /// 画面表示中の画像を差し替える
     func changeImge() {
         if isShuffle {
             self.myView.image = UIImage(named: "card_draw")
             self.view.backgroundColor = UIColor(red: 143/255, green: 180/255, blue: 139/255, alpha: 1.0)
             isShuffle = false
-        }else {
+        } else {
             self.myView.image = UIImage(named: "card_shuffle")
             self.view.backgroundColor = UIColor(red: 175/255, green: 117/255, blue: 96/255, alpha: 1.0)
             isShuffle = true
         }
+        
     }
 }
 
